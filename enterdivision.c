45072e7067f2c3728a6b6c23227c1a7f6ee81529
VU21CSEN0101621 <95604026+VU21CSEN0101621@users.noreply.github.com>
2021-12-06 10:41:59 +0530

    Add files via upload

---
+++ enterdivision.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	float c;
+	printf("enter first number");
+	scanf("%d",&a);
+	printf("enter second number");
+	scanf("%d",&b);
+	c=(float)a/b;
+	printf("division is %f",c);
+}
