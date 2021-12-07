2137809141211c63d738e6c828956ec67001729b
VU21CSEN0101621 <95604026+VU21CSEN0101621@users.noreply.github.com>
2021-12-07 15:29:12 +0530

    Add files via upload

---
+++ postiveinteger.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	scanf("%d",&a);
+       if(a>=0)
+       {
+       	printf("the number is postive");
+	}
+	else
+	{
+		printf("the number is negative");
+	}	
+}
+       	
+	
