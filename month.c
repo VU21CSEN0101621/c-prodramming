68d495748141292da7ea1412d5cbab6e463fd52e
VU21CSEN0101621 <95604026+VU21CSEN0101621@users.noreply.github.com>
2021-12-07 15:35:13 +0530

    Add files via upload

---
+++ month.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	scanf("%d",&a);
+       if(a>=1&&a<=12)
+       {
+       	printf("the number is a month number");
+	}
+	else
+	{
+		printf("the number isnt a month number");
+	}	
+}
