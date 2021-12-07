7e330f8d1abc0e4da159f58502bb37995eb84e41
VU21CSEN0101621 <95604026+VU21CSEN0101621@users.noreply.github.com>
2021-12-07 15:29:39 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	char x;
+	scanf("%c",&x);
+	if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+	{
+		printf("x is  a vowel");
+	}
+	else
+	{
+		printf("x is  a consonant");
+	}
+}
