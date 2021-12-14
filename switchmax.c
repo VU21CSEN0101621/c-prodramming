51e0f19c034904ad64a12715bb9b91303149bc33
VU21CSEN0101621 <95604026+VU21CSEN0101621@users.noreply.github.com>
2021-12-14 16:49:18 +0530

    Add files via upload

---
+++ switchmax.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	switch(a>b)
+	{
+		case 1:printf("a is max");
+		break;
+		case 0:printf("b is max");
+		break;
+		default:printf("both are equal");
+	}	
+}
