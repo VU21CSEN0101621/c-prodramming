84c632cc1f1093f1bf10a85dc4f3f9620839800c
VU21CSEN0101621 <95604026+VU21CSEN0101621@users.noreply.github.com>
2021-12-27 10:16:28 +0530

    Add files via upload

---
+++ multiplesof5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	    int i=650;
+	    while(i>900)
+	    {
+	     if(i%5==0)
+		{
+		printf("%d\n",i);
+		i++;
+	    }
+        }                                                                                                                                                                                                                                                                                                                                                                                                                         }
+}
