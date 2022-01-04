658119743ed0103d85fdabc830fff4ed820cb1b6
VU21CSEN0101621 <95604026+VU21CSEN0101621@users.noreply.github.com>
2022-01-04 23:09:50 +0530

    Update multiplesof5.c

--- multiplesof5.c
+++ multiplesof5.c
@@ -2,12 +2,10 @@
 int main()
 {
 	    int i=650;
-	    while(i>900)
+	    while(i<900)
 	    {
 	     if(i%5==0)
-		{
 		printf("%d\n",i);
 		i++;
-	    }
-        }                                                                                                                                                                                                                                                                                                                                                                                                                         }
+ }                                         
 }
